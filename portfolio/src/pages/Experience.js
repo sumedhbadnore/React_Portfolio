7c37815c78b90d5452e7c0f7ea511e723b625a43
Sumedh Badnore <sumedhbadnore@Sumedhs-MacBook-Air.local>
2024-10-29 02:45:38 -0400

    added home page

--- portfolio/src/pages/Experience.js
+++ portfolio/src/pages/Experience.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
+import { School, Work, Web } from "@mui/icons-material";
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+  <div className="experience"> 
+    <VerticalTimeline lineColor="#3e497a">
+      <VerticalTimelineElement className="vertical-timeline-element--education" date="2023 - 2025" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<School/>}>
+        <h3 className="vertical-timeline-element-title"> Stevens Institute of Technology, Hoboken, NJ</h3>
+        <p> Masters in Computer Science </p>
+        <i> GPA 3.8/4 </i>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement className="vertical-timeline-element--education" date="Aug 2022 - Dec 2022" iconStyle={{background: '#FFA500', color: '#fff'}} icon={<Work/>}>
+        <h3 className="vertical-timeline-element-title"> Accenture, Pune, India</h3>
+        <p> Developer Program Intern </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement className="vertical-timeline-element--education" date="May 2022 - Aug 2022" iconStyle={{background: '#FFA500', color: '#fff'}} icon={<Web/>}>
+        <h3 className="vertical-timeline-element-title"> Vidyalankar Institute of Technology, Mumbai, India</h3>
+        <p> Web Development Intern </p>
+        <i> CovInfo Web App </i>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement className="vertical-timeline-element--education" date="2019 - 2023" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<School/>}>
+        <h3 className="vertical-timeline-element-title"> Vidyalankar Institute of Technology, Mumbai, India</h3>
+        <p> Bachelors in Electronics and Communication Engineering </p>
+        <i> GPA 3.7/4 </i>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  </div>
+  );
 }
 
-export default Experience
+export default Experience;
